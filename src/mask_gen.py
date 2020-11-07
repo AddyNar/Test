efcb9f2545c4f9822de117336ef35cf9bc41c961
Adithya Narayan <30385650+Alphaepsiolon@users.noreply.github.com>
2020-11-08 00:25:07 +0530

    Fixed colour_mask generation in maskGen
    Fixed the colour_mask method in the maskGen class to ensure that it
    generates the correct output. Also, added display files to the same.

--- src/mask_gen.py
+++ src/mask_gen.py
@@ -1,15 +1,16 @@
 import cv2 as cv
-from cspace_change.py import ColourSpaceChanger
+from cspace_change import ColourSpaceChanger
 import os
+import numpy as np
 
 class maskGen(object):
     """
     Class which contains various image mask generation functions.
     """
-    def __init__(dims):
+    def __init__(self, dims):
         self.dims = dims
 
-    def colour_mask(slf, img, upper_thresh, lower_thresh):
+    def colour_mask(self, img, upper_thresh, lower_thresh):
         """
         Generate a mask for three channel images. By default, images channels
         are assumed to be in (B,G,R) as is default in OpenCV.
@@ -21,6 +22,7 @@ class maskGen(object):
         """
         # Convert colourspace from BGR to HSV
         spc = ColourSpaceChanger(self.dims)
+        img = cv.resize(img, self.dims, interpolation = cv.INTER_NEAREST)
         hsv = spc.space_change(img, "BGR", "HSV")
 
         # Generate a mask with the given RGB bounds
@@ -46,8 +48,8 @@ class maskGen(object):
 
 if __name__ == "__main__":
     base_dir = "../Data"
-    img = cv.imread(os.path.join(base_dir, "lena.jpg"))
-    mask = maskGen(dims=(256, 256))
-    out = mask.colour_mask()
-    cv.imshow(out)
+    img = cv.imread(os.path.join(base_dir, "house.jpg"))
+    mask = maskGen((256, 256))
+    out = mask.colour_mask(img, np.array([100, 100, 100]), np.array([255, 255, 255]))
+    cv.imshow('frame',out)
     cv.waitKey()
